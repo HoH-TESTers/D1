9167d4028c5ca51440c90998c223a731f76e561b
HoH-TESTers <montrosedwight@gmail.com>
2026-02-10 14:01:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,36 +4,6 @@ import random
 import re
 import os
 
-# --- 1. PASSWORD PROTECTION ---
-def check_password():
-    """Returns True if the user had the correct password."""
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == "Dh0l#isabh0l3":  ## <--- CHANGE YOUR PASSWORD HERE
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
-        else:
-            st.session_state["password_correct"] = False
-
-    if "password_correct" not in st.session_state:
-        # First run, show input for password.
-        st.text_input(
-            "Enter Password to access the Quiz", type="password", on_change=password_entered, key="password"
-        )
-        return False
-    elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input(
-            "Enter Password to access the Quiz", type="password", on_change=password_entered, key="password"
-        )
-        st.error("😕 Password incorrect")
-        return False
-    else:
-        # Password correct.
-        return True
-
-if not check_password():
-    st.stop()  # Halt execution until password is correct
 
 # --- 2. LOAD EXCEL FILE ---
 @st.cache_data
